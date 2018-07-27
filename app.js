d82d853fbeadc769c314d96b762d79f965724049
amartincastro <castroarielm@gmail.com>
2018-07-27 11:11:42 -0700

    JSON.stringify to pretty print geo data

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const request = require('request');
 
-request({}, (error, response, body) => {
-
+request({
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+  json: true
+}, (error, response, body) => {
+  console.log(JSON.stringify(body, undefined, 2));
 });
