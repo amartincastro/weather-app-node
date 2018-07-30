bef64ac59f3092d398036381289e84ec33d54092
amartincastro <castroarielm@gmail.com>
2018-07-30 15:29:54 -0700

    require yargs and configure argv to enable user input from CLI

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
 
 request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
@@ -6,5 +21,5 @@ request({
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);
   console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.long}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
