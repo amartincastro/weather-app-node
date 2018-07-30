1005c28ab9bd22a64ed49b7c4b5951945f376b63
amartincastro <castroarielm@gmail.com>
2018-07-30 15:03:59 -0700

    testing Google Maps API

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
   json: true
 }, (error, response, body) => {
-  console.log(body, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Address: ${body.results[1].[0].lat}`)
 });
