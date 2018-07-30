5ff309a2920eec9af7dd5000c9829d233ad52aee
amartincastro <castroarielm@gmail.com>
2018-07-30 15:05:20 -0700

    access Lat and Long data

--- app.js
+++ app.js
@@ -5,5 +5,6 @@ request({
   json: true
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Address: ${body.results[1].[0].lat}`)
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.long}`);
 });
