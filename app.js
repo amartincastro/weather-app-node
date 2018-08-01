04e00d4d373e7f1d46c25be0e5375475cb51855c
amartincastro <castroarielm@gmail.com>
2018-08-01 15:54:47 -0700

    write error handling for issues with Dark Sky API server

--- app.js
+++ app.js
@@ -35,5 +35,8 @@ request({
   url: 'https://api.darksky.net/forecast/1bfe917aacd88e20fd24bb63ecf35918/39.524489,-119.717457',
   json: true
 }, (error, response, body) => {
+  if (error) {
+    console.log('Unable to connect to the Dark Sky API servers.');
+  }
   console.log(body.currently.temperature);
 });
