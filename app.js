420ecdaf431fbdf2498ccc2275987fc24e061b09
amartincastro <castroarielm@gmail.com>
2018-08-01 16:02:29 -0700

    refactor and clean up error handling

--- app.js
+++ app.js
@@ -35,10 +35,9 @@ request({
   url: 'https://api.darksky.net/forecast/1bfe917aacd88e20fd24bb63ecf35918/39.524489,-119.717457',
   json: true
 }, (error, response, body) => {
-  if (error) {
-    console.log('Unable to connect to the Dark Sky API servers.');
-  } else if (response.statusCode == 400) {
+  if (!error && response.statusCode === 200) {
+    console.log(body.currently.temperature);
+  } else {
     console.log('Unable to fetch weather.');
   }
-  console.log(body.currently.temperature);
-});
+  });
