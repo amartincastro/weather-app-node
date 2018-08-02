bce05f8350c9fb6c7c26009f952b8fdfe1dc960e
amartincastro <castroarielm@gmail.com>
2018-08-02 15:52:31 -0700

    Take data from geocodeAddress and pass into getWeather

--- app.js
+++ app.js
@@ -17,21 +17,20 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//   if (errorMessage) {
-//     console.log(errorMessage);
-//   } else {
-//     console.log(JSON.stringify(results, undefined, 2));
-//   }
-// });
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(results.address);
+    weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+      if (error) {
+        console.log(errorMessage);
+      } else {
+        console.log(JSON.stringify(weatherResults, undefined, 2));
+      }
+    });
+  }
+});
 
 
 //pass in lat, lng, callback
-weather.getWeather(39.524489, -119.717457, (errorMessage, weatherResults) => {
-  if (error) {
-    console.log(errorMessage);
-  } else {
-    console.log(JSON.stringify(weatherResults, undefined, 2));
-  }
-
-});
