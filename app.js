90c18b0ebaf670143799bd8988313de887ffa867
amartincastro <castroarielm@gmail.com>
2018-08-02 15:55:16 -0700

    Add temperature and apparentTemperature to CLI output

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
       if (error) {
         console.log(errorMessage);
       } else {
-        console.log(JSON.stringify(weatherResults, undefined, 2));
+        console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}.`);
       }
     });
   }
